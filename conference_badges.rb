a27208ccdc67e7b0531bdca4b64f267b9380c695
Mike Wilder <mikewilder01@gmail.com>
2020-10-21 23:04:22 -0400

    commit tree ad552f86f81dec360eed8f7753b7ce3740f0d9dd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badges = []
+    array.each do |name|
+        badges << "Hello, my name is #{name}."
+    end 
+    return badges
+end
+
+def assign_rooms(array)
+    assignments = []
+    array.each.with_index(1) do | name, index |
+        assignments << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end 
+    return assignments
+end 
+
+def printer(array)
+    batch_badge_creator(array).each do |hello|
+      puts hello
+    end 
+    
+    assign_rooms(array).each do |room|
+      puts room 
+    end
+end 
